d834b7d8ecd1cc702e57c2d74271eb94f7ff0a07
oscar <druiventakoscar@gmail.com>
2018-06-28 18:00:05 +0200

    add tslint and fix linting errors

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-'use strict'
-import omit from 'lodash.omit';
-import pick from 'lodash.pick';
-import { Model } from 'objection';
+"use strict";
+import omit from "lodash.omit";
+import pick from "lodash.pick";
+import { Model } from "objection";
 
 interface IConstructor extends Function {
   hidden: string[];
@@ -9,23 +9,23 @@ interface IConstructor extends Function {
 }
 
 export default (ModelClass: typeof Model) => {
-  return <typeof Model>class extends ModelClass {
-    $formatJson (json: {}) {
-      let superJson = super.$formatJson(json)
+  return class extends ModelClass {
+    public $formatJson(json: {}) {
+      let superJson = super.$formatJson(json);
 
-      let conf = this.constructor as IConstructor;
+      const conf = this.constructor as IConstructor;
 
-      if (!conf.hidden && !conf.visible) return superJson
+      if (!conf.hidden && !conf.visible) { return superJson; }
 
       if (conf.visible) {
-        superJson = pick(superJson, conf.visible)
+        superJson = pick(superJson, conf.visible);
       }
 
       if (conf.hidden) {
-        superJson = omit(superJson, conf.hidden)
+        superJson = omit(superJson, conf.hidden);
       }
 
-      return superJson
+      return superJson;
     }
-  }
-}
+  } as typeof Model;
+};
--- test/test.ts
+++ test/test.ts
@@ -1,139 +1,140 @@
-'use strict'
-import visibilityPlugin from '../src';
-import { expect } from 'chai';
-import { Model } from 'objection';
-import Knex from 'knex';
+"use strict";
+// tslint:disable:max-classes-per-file
+import { expect } from "chai";
+import Knex from "knex";
+import { Model } from "objection";
+import visibilityPlugin from "../src";
 
-describe('objection-timestamp test', () => {
+describe("objection-timestamp test", () => {
   let knex: Knex;
 
   before(() => {
     knex = Knex({
-      client: 'sqlite3',
+      client: "sqlite3",
       useNullAsDefault: true,
       connection: {
-        filename: './test.db'
-      }
-    })
-  })
+        filename: "./test.db",
+      },
+    });
+  });
 
   before(() => {
     return Promise.all([
-      knex.schema.createTable('user', table => {
-        table.increments('id').primary()
-        table.string('firstName')
-        table.string('lastName')
-        table.string('hashedPassword')
+      knex.schema.createTable("user", (table) => {
+        table.increments("id").primary();
+        table.string("firstName");
+        table.string("lastName");
+        table.string("hashedPassword");
       }),
-      knex.schema.createTable('post', table => {
-        table.increments('id').primary()
-        table.string('title')
-        table.string('slug')
-        table.string('description')
+      knex.schema.createTable("post", (table) => {
+        table.increments("id").primary();
+        table.string("title");
+        table.string("slug");
+        table.string("description");
       }),
-    ])
-  })
+    ]);
+  });
 
   after(() => {
     return Promise.all([
-      knex.schema.dropTable('user'),
-      knex.schema.dropTable('post')
-    ])
-  })
+      knex.schema.dropTable("user"),
+      knex.schema.dropTable("post"),
+    ]);
+  });
 
   after(() => {
-    return knex.destroy()
-  })
+    return knex.destroy();
+  });
 
   beforeEach(() => {
     return Promise.all([
-      knex('user').delete(),
-      knex('post').delete()
-    ])
-  })
+      knex("user").delete(),
+      knex("post").delete(),
+    ]);
+  });
 
-  it('should not show blacklisted properties when serialized to json', () => {
+  it("should not show blacklisted properties when serialized to json", () => {
     class User extends visibilityPlugin(Model) {
-      firstName!: string;
-      lastName!: string;
-      hashedPassword!: string;
-      static get tableName () {
-        return 'user'
+      public firstName!: string;
+      public lastName!: string;
+      public hashedPassword!: string;
+      static get tableName() {
+        return "user";
       }
-      static get hidden () {
-        return ['hashedPassword']
+      static get hidden() {
+        return ["hashedPassword"];
       }
     }
 
     return User
       .query(knex)
-      .insert({firstName: 'John', lastName: 'Doe', hashedPassword: 'my secret'})
-      .then(john => {
-        expect(john).to.have.property("hashedPassword")
-        expect(john.toJSON()).to.not.have.property("hashedPassword")
-      })
-  })
+      .insert({firstName: "John", lastName: "Doe", hashedPassword: "my secret"})
+      .then((john) => {
+        expect(john).to.have.property("hashedPassword");
+        expect(john.toJSON()).to.not.have.property("hashedPassword");
+      });
+  });
 
-  it('should only show whitelisted properties when serialized to json', () => {
+  it("should only show whitelisted properties when serialized to json", () => {
     class User extends visibilityPlugin(Model) {
-      firstName!: string;
-      lastName!: string;
-      hashedPassword!: string;
-      static get tableName () {
-        return 'user'
+      public firstName!: string;
+      public lastName!: string;
+      public hashedPassword!: string;
+      static get tableName() {
+        return "user";
       }
-      static get visible () {
-        return ['firstName', 'id']
+      static get visible() {
+        return ["firstName", "id"];
       }
     }
 
     return User
       .query(knex)
-      .insert({firstName: 'jane', lastName: 'Doe', hashedPassword: 'my secret2'})
-      .then(jane => {
-        let serialized = jane.toJSON()
-        expect(serialized).to.have.property("firstName", "jane")
-        expect(serialized).to.have.property("id")
-        expect(serialized).to.not.have.property("lastName")
-        expect(serialized).to.not.have.property("hashedPassword")
-      })
-  })
+      .insert({firstName: "jane", lastName: "Doe", hashedPassword: "my secret2"})
+      .then((jane) => {
+        const serialized = jane.toJSON();
+        expect(serialized).to.have.property("firstName", "jane");
+        expect(serialized).to.have.property("id");
+        expect(serialized).to.not.have.property("lastName");
+        expect(serialized).to.not.have.property("hashedPassword");
+      });
+  });
 
-  it('should work when applying to a base class model', () => {
+  it("should work when applying to a base class model", () => {
     class Base extends visibilityPlugin(Model) {}
 
     class User extends Base {
-      firstName!: string;
-      lastName!: string;
-      hashedPassword!: string;
-      static get tableName () {
-        return 'user'
+      public firstName!: string;
+      public lastName!: string;
+      public hashedPassword!: string;
+      static get tableName() {
+        return "user";
       }
-      static get hidden () {
-        return ['hashedPassword', 'id']
-      } 
+      static get hidden() {
+        return ["hashedPassword", "id"];
+      }
     }
 
     class Post extends Base {
-      title!: string;
-      static get tableName () {
-        return 'post'
+      public title!: string;
+      static get tableName() {
+        return "post";
       }
     }
 
     return User
       .query(knex)
-      .insert({firstName: 'joe', lastName: 'doe', hashedPassword: 'another secret'})
-      .then(joe => {
-        let serialized = joe.toJSON()
-        expect(serialized).to.not.have.property("hashedPassword")
-        expect(serialized).to.not.have.property("id")
+      .insert({firstName: "joe", lastName: "doe", hashedPassword: "another secret"})
+      .then((joe) => {
+        const serialized = joe.toJSON();
+        expect(serialized).to.not.have.property("hashedPassword");
+        expect(serialized).to.not.have.property("id");
       })
-      .then(() => Post.query(knex).insert({ title: 'my first post' }))
-      .then(post => {
-        let serialized = post.toJSON()
-        expect(serialized).to.have.property("id")
-        expect(serialized).to.have.property("title")
-      })
-  })
-})
+      .then(() => Post.query(knex).insert({ title: "my first post" }))
+      .then((post) => {
+        const serialized = post.toJSON();
+        expect(serialized).to.have.property("id");
+        expect(serialized).to.have.property("title");
+      });
+  });
+});
